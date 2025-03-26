4769ca462291657cb9c49c9b4c722c688d51fa90
gnuoynawh <bizxhy@gmail.com>
2025-03-26 19:58:14 +0900

    dependencies 추가 (cameraX, ml-kit, glide)

--- TicketDiary/TextReaderComposeSample/app/build.gradle.kts
+++ TicketDiary/TextReaderComposeSample/app/build.gradle.kts
@@ -66,4 +66,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+
+    implementation(libs.google.mlkit)
+    implementation(libs.google.mlkit.korean)
+
+    implementation(libs.github.glide)
 }
