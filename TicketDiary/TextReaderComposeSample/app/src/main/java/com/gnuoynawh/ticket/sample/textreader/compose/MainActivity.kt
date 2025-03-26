2c1ffc6b592426d02913622499f2e375a98c106d
gnuoynawh <bizxhy@gmail.com>
2025-03-26 19:50:02 +0900

    샘플 프로젝트 생성. Text Reader with Compose

---
+++ TicketDiary/TextReaderComposeSample/app/src/main/java/com/gnuoynawh/ticket/sample/textreader/compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.gnuoynawh.ticket.sample.textreader.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gnuoynawh.ticket.sample.textreader.compose.ui.theme.TextReaderComposeSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TextReaderComposeSampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TextReaderComposeSampleTheme {
+        Greeting("Android")
+    }
+}
