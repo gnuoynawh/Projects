9ac74670d354a9af4c01a1d64843c9c5666dfae6
gnuoynawh <bizxhy@gmail.com>
2025-03-26 20:27:43 +0900

    카메라 권한, 카메라 Preview 화면 추가

--- TicketDiary/TextReaderComposeSample/app/src/main/java/com/gnuoynawh/ticket/sample/textreader/compose/MainActivity.kt
+++ TicketDiary/TextReaderComposeSample/app/src/main/java/com/gnuoynawh/ticket/sample/textreader/compose/MainActivity.kt
@@ -1,47 +1,65 @@
 package com.gnuoynawh.ticket.sample.textreader.compose
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.gnuoynawh.ticket.sample.textreader.compose.ui.CameraScreen
 import com.gnuoynawh.ticket.sample.textreader.compose.ui.theme.TextReaderComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                setCameraPreview()
+            } else {
+                // Camera permission denied (Handle denied operation)
+            }
+
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TextReaderComposeSampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+        // 권한체크
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                setCameraPreview()
+            }
+            else -> {
+                cameraPermissionRequest.launch(Manifest.permission.CAMERA)
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TextReaderComposeSampleTheme {
-        Greeting("Android")
+    private fun setCameraPreview() {
+        enableEdgeToEdge()
+        setContent {
+            TextReaderComposeSampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraScreen()
+                }
+            }
+        }
     }
 }
